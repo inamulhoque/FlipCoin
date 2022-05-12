0cb43a08e533ab25602762ebbafa58586e3d57a2
Inamul <inamulhoque097@gmail.com>
2022-05-12 14:03:14 +0530

    Update: Displaying winning of Head and Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
-val=$((RANDOM%2))
-if [ $val -eq 1 ]
-then
-echo "It's a Head"
-else
-echo "It's Tail"
-fi
+win=0
+loss=0
+
+for (( i=0; i<10; i++ ))
+do
+	val=$((RANDOM%2))
+	if [ $val -eq 1 ]
+	then
+		((win++))
+	else
+		((loss++))
+	fi
+done
+echo "Total 10 number of toss"
+echo "Head wins : $win"
+echo "Tail wins : $loss"
