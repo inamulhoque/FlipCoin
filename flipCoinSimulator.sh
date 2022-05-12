8e4e36460c0605a415f0f0d7f101520173654d23
Inamul <inamulhoque097@gmail.com>
2022-05-12 19:20:48 +0530

    Modified: if game ties then game continues

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -12,6 +12,7 @@ do
 		((loss++))
 	fi
 done
+
 echo "Head wins : $win"
 echo "Tail wins : $loss"
 
@@ -26,4 +27,24 @@ then
 elif [[ $win -eq $loss ]]
 then
 	echo "Tie"
+	while [[ $win -eq $loss ]]
+	do
+		val2=$((RANDOM%2))
+        	if [ $val2 -eq 1 ]
+        	then
+                	((win++))
+        	else
+                	((loss++))
+        	fi
+	done
+	if [[ $win -gt $loss ]]
+	then
+        	echo "Head Wins by :"
+        	echo "$(($win - $loss))"
+	elif [[ $loss -gt $win ]]
+	then
+        	echo "Tail wins by :"
+        	echo "$(($loss - $win))"
+	fi
+
 fi
